cf366333750f11525dc63d19d8a47d014a480d8d
Corinna Bödigheimer <corinna.boedigheimer@gmx.de>
2024-11-18 15:16:53 +0100

    Basis Transaction - Corinna

--- backend-money-maker/src/main/java/org/example/backendmoneymaker/entities/Transaction.java
+++ backend-money-maker/src/main/java/org/example/backendmoneymaker/entities/Transaction.java
@@ -1,4 +1,31 @@
 package org.example.backendmoneymaker.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name="transactions")
+@Data
 public class Transaction {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private float amount;
+    private Date timestamp;
+    private String description;
+
+    //private Category category;
+    //private Account account;
+
+
+
+
+
+
+
+
+
 }
