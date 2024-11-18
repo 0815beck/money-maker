0d28aa7a196497032703bd1cf651fe433a536cf1
RonjaPieroth <ronja.pieroth@gmail.com>
2024-11-18 15:47:57 +0100

    added Service Interfaces for Category and Account

--- backend-money-maker/src/main/java/org/example/backendmoneymaker/services/AccountService.java
+++ backend-money-maker/src/main/java/org/example/backendmoneymaker/services/AccountService.java
@@ -1,4 +1,17 @@
 package org.example.backendmoneymaker.services;
 
+import org.example.backendmoneymaker.entities.Account;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountService {
+
+    List<Account> getAllAccounts();
+
+    Optional<Account> getAccountById(Long id);
+
+    void deleteAccount(Long id);
+
+    Account updateAccount(Account account);
 }
