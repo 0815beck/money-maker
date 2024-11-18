051cc806d0010642eeaa3c05436c3e792348bf8e
RonjaPieroth <ronja.pieroth@gmail.com>
2024-11-18 15:59:06 +0100

    implemented Service Interfaces for Services of Category & Account

--- backend-money-maker/src/main/java/org/example/backendmoneymaker/services/CategoryServiceImpl.java
+++ backend-money-maker/src/main/java/org/example/backendmoneymaker/services/CategoryServiceImpl.java
@@ -1,4 +1,31 @@
 package org.example.backendmoneymaker.services;
 
-public class CategoryServiceImpl {
+
+import lombok.Data;
+import org.example.backendmoneymaker.entities.Category;
+import org.example.backendmoneymaker.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Data
+public class CategoryServiceImpl implements CategoryService {
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
 }
