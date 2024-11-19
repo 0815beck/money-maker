5dabaf9afb3418467ded7679758989cf4831c30f
RonjaPieroth <ronja.pieroth@gmail.com>
2024-11-19 16:19:05 +0100

    filled in interfaces category & account + added structure for services

--- frontend-money-maker/src/app/models/account.ts
+++ frontend-money-maker/src/app/models/account.ts
@@ -1,2 +1,9 @@
+import {FixedCost} from './fixed-cost';
+import {Transaction} from './transaction';
+
 export interface Account {
+  id?: number;
+  name: string;
+  fixedCosts: FixedCost[];
+  transactions: Transaction[];
 }
