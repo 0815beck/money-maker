e43e4f05b766e4d083ffa4c69a34115691d27251
Corinna Bödigheimer <corinna.boedigheimer@gmx.de>
2024-11-19 16:18:46 +0100

    Interface Transaction

--- frontend-money-maker/src/app/models/transaction.ts
+++ frontend-money-maker/src/app/models/transaction.ts
@@ -1,2 +1,11 @@
+import { Account } from './account';
+import { Category } from './category';
+
 export interface Transaction {
+  id?: number;
+  amount: number;
+  timestamp: Date;
+  description?: string;
+  category: Category;
+  account: Account;
 }
