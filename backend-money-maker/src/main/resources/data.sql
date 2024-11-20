2b49f24ad306666ecaa994985114a51c43b11ad9
Corinna Bödigheimer <corinna.boedigheimer@gmx.de>
2024-11-20 14:48:24 +0100

    Transaction From Base

--- backend-money-maker/src/main/resources/data.sql
+++ backend-money-maker/src/main/resources/data.sql
@@ -2,5 +2,8 @@ INSERT INTO account (name) VALUES ('Nico');
 INSERT INTO account (name) VALUES ('Corinna');
 INSERT INTO account (name) VALUES ('Ronja');
 
+INSERT INTO category (name) VALUES('Lebensmittel');
+INSERT INTO category (name) VALUES('Spielzeug');
+
 
 
