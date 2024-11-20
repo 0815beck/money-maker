7e342fe2ac93ed2ef92ece4e60d4dbd586666506
RonjaPieroth <ronja.pieroth@gmail.com>
2024-11-20 16:48:23 +0100

    + create Fix cost from form

--- frontend-money-maker/src/app/components/fixed-cost-form/fixed-cost-form.component.ts
+++ frontend-money-maker/src/app/components/fixed-cost-form/fixed-cost-form.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AccountService} from '../../services/account.service';
 import {Account} from '../../models/account';
 import {CategoryService} from '../../services/category.service';
 import {Category} from '../../models/category';
 import {FixedCost} from '../../models/fixed-cost';
+import {FixedCostService} from '../../services/fixed-cost.service';
+import {TransactionService} from '../../services/transaction.service';
+import {Transaction} from '../../models/transaction';
 
 @Component({
   selector: 'app-fixed-cost-form',
@@ -13,29 +16,44 @@ import {FixedCost} from '../../models/fixed-cost';
 })
 export class FixedCostFormComponent {
 
-  today:string;
+  today: string;
   fixedCostForm: FormGroup;
   account?: Account;
   categoryList: Category[] = []
 
-  constructor(private fb: FormBuilder, private accountService: AccountService, private categoryService: CategoryService) {
+  constructor(private fb: FormBuilder, private accountService: AccountService, private categoryService: CategoryService, private fixedCostService: FixedCostService, private transactionService: TransactionService) {
     this.today = new Date().toISOString().split('T')[0];
     accountService.account$.subscribe(data => this.account = data);
     categoryService.getCategories().subscribe(data => this.categoryList = data);
     this.fixedCostForm = this.fb.group(
-      {amount: ["", Validators.required],
+      {
+        amount: ["", Validators.required],
         start: ["", Validators.required],
         description: [""],
-        category:["", Validators.required],
-        account:[this.account],
-        generatedTransactions:[[]]
+        category: ["", Validators.required],
+        account: [this.account],
+        generatedTransactions: [[]]
       }
     )
   }
 
-  createFixedCost(){
+  createFixedCost(): void {
     const fixedCost: FixedCost = this.fixedCostForm.value;
-
+    if (fixedCost.start === new Date()) {
+      let transaction: Transaction = {
+        amount: fixedCost.amount,
+        account: fixedCost.account,
+        category: fixedCost.category,
+        description: fixedCost.description,
+        timestamp: new Date()
+      }
+      this.transactionService.addTransaction(transaction).subscribe(data => transaction = data);
+      fixedCost.generatedTransactions.push(transaction);
+    }
+    this.fixedCostService.addFixedCost(fixedCost).subscribe(() => {
+      this.accountService.fetchAccounts();
+      this.accountService.refetchSelectedAccount()
+    });
   }
 
 }
