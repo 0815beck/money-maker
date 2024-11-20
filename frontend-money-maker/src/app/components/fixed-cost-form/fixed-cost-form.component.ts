968c3fb402975596bd5227d3094cf71d6b66d2b0
RonjaPieroth <ronja.pieroth@gmail.com>
2024-11-20 16:22:50 +0100

    worked on form in fixed costs(formgroup)

--- frontend-money-maker/src/app/components/fixed-cost-form/fixed-cost-form.component.ts
+++ frontend-money-maker/src/app/components/fixed-cost-form/fixed-cost-form.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AccountService} from '../../services/account.service';
 import {Account} from '../../models/account';
+import {CategoryService} from '../../services/category.service';
+import {Category} from '../../models/category';
+import {FixedCost} from '../../models/fixed-cost';
 
 @Component({
   selector: 'app-fixed-cost-form',
@@ -13,19 +16,26 @@ export class FixedCostFormComponent {
   today:string;
   fixedCostForm: FormGroup;
   account?: Account;
+  categoryList: Category[] = []
 
-  constructor(private fb: FormBuilder, accountService: AccountService) {
+  constructor(private fb: FormBuilder, private accountService: AccountService, private categoryService: CategoryService) {
     this.today = new Date().toISOString().split('T')[0];
     accountService.account$.subscribe(data => this.account = data);
+    categoryService.getCategories().subscribe(data => this.categoryList = data);
     this.fixedCostForm = this.fb.group(
       {amount: ["", Validators.required],
-        start: [new Date(), Validators.required],
+        start: ["", Validators.required],
         description: [""],
         category:["", Validators.required],
         account:[this.account],
         generatedTransactions:[[]]
       }
     )
+  }
+
+  createFixedCost(){
+    const fixedCost: FixedCost = this.fixedCostForm.value;
+
   }
 
 }
