0e1c88174d9e586ec32d33bd5df95cc4f669c11c
Corinna Bödigheimer <corinna.boedigheimer@gmx.de>
2024-11-20 15:25:27 +0100

    Transaction From Modal

--- frontend-money-maker/src/app/components/transaction-form/transaction-form.component.ts
+++ frontend-money-maker/src/app/components/transaction-form/transaction-form.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-transaction-form',
   templateUrl: './transaction-form.component.html',
-  styleUrl: './transaction-form.component.css'
+  styleUrl: './transaction-form.component.css',
 })
 export class TransactionFormComponent {
+  categoryList!: Category[];
 
+  constructor(private categoryService: CategoryService) {}
+
+  loadCategories() {
+    this.categoryService.getCategories().subscribe({
+      next: (data) => {
+        this.categoryList = data;
+      },
+      error: (error) => {
+        console.log('Fehler beim Laden der Categorien: ', error);
+      },
+    });
+  }
 }
