f65ac2d957cb32f757505222e457b4890af166bd
RonjaPieroth <ronja.pieroth@gmail.com>
2024-11-20 16:35:11 +0100

    added basic http requests for Transaction service

--- frontend-money-maker/src/app/services/transaction.service.ts
+++ frontend-money-maker/src/app/services/transaction.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Transaction } from '../models/transaction';
-import { BehaviorSubject, flatMap, Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {env} from '../env';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +12,29 @@ export class TransactionService {
   _transactions = new BehaviorSubject<Transaction[]>([]);
   transactions$ = this._transactions.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getTransactions() : Observable<Transaction[] | undefined> {
-    return this.transactions$;
+  getTransactions() : Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(env.baseUrl + "/transaction");
   }
+
+  addTransaction(transaction: Transaction): Observable<Transaction>{
+    return this.http.post<Transaction>(env.baseUrl + "/transaction", transaction);
+  }
+
+  modifyTransaction(transaction: Transaction): Observable<Transaction>{
+    return this.http.put<Transaction>(env.baseUrl + "/transaction", transaction);
+  }
+
+  deleteTransaction(id: number): Observable<void>{
+    return this.http.delete<void>(env.baseUrl + "/transaction/"+id);
+  }
+
 //FOR TESTING PURPOSES
   getTotalIncome() {
     return new Observable<number | undefined>((observer) => {
       observer.next(200);
-    })  
+    })
   }
 
   getTotalExpense() {
