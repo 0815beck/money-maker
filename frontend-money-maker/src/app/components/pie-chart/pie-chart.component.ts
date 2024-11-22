5559e031ab0a081b2f6f1697bdf99a85afa99169
Nico Beck <nicobeck97@gmail.com>
2024-11-22 15:29:09 +0100

    dataset pie chart comes from parent component

--- frontend-money-maker/src/app/components/pie-chart/pie-chart.component.ts
+++ frontend-money-maker/src/app/components/pie-chart/pie-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import Chart from 'chart.js/auto';
 
 @Component({
@@ -7,12 +7,43 @@ import Chart from 'chart.js/auto';
   styleUrl: './pie-chart.component.css'
 })
 export class PieChartComponent {
+
+  @Input() data: any;
+  
   chart: any = [];
+  colors = [
+    'rgb(255, 25, 75)',
+    'rgb(0, 233, 225)',
+    'rgb(253, 235, 35)',
+    'rgb(9, 185, 185)',
+    'rgb(116, 49, 249)',
+    'rgb(250, 140, 29)'
+  ];
 
   constructor() {}
 
   ngOnInit() {
-    Chart.defaults.color = '#501a56';
+    /* if (!this.data) {
+      console.log('[Error] data is undefined');
+      return;
+    }
+    
+    this.data!.datasets[0]!.backgroundColor = this.colors;
+
+    const chartConfig = {
+      type: 'pie',
+      data: this.data,
+      options: {
+        responsive: true,
+        scales: {
+          x: { beginAtZero: true },
+          y: { beginAtZero: true,},
+        },
+      }
+    }
+
+    this.chart = new Chart('canvas', chartConfig);
+
     this.chart = new Chart('canvas', {
       type: 'pie',
       data: {
@@ -34,12 +65,39 @@ export class PieChartComponent {
         ],
       },
       options: {
+        responsive: true,
         scales: {
           y: {
             beginAtZero: true,
           },
         },
       },
-    });
+    });*/
+  }
+
+  ngOnChanges() {
+    if (!this.data) {
+      console.log('[Error] data is undefined');
+      return;
+    }
+
+    this.data.datasets[0].backgroundColor = this.colors;
+    this.data.datasets[0].borderWidth = 1;
+
+    const chartConfig = {
+      type: 'pie',
+      data: this.data,
+      options: {
+        responsive: true,
+        scales: {
+          x: { beginAtZero: true },
+          y: { beginAtZero: true,},
+        },
+      }
+    }
+
+    this.chart = new Chart('canvas', chartConfig);
+
+    console.log('[Debug] Something happened, the input data is now: ', this.data);
   }
 }
---
+++ frontend-money-maker/src/app/models/pie-chart-data.ts
@@ -0,0 +1,4 @@
+export interface PieChartData {
+    labels: string[],
+    datasets: { label: string, data: number[] }[]
+}
