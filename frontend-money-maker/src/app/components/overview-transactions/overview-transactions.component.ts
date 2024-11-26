ba249101adf4f63a9ef5b9d1ccdfdc118a89ea01
Corinna <corinna.boedigheimer@gmx.de>
2024-11-26 16:13:29 +0100

    Delete Transaction (error)

--- frontend-money-maker/src/app/components/overview-transactions/overview-transactions.component.ts
+++ frontend-money-maker/src/app/components/overview-transactions/overview-transactions.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Category } from '../../models/category';
 import { CategoryService } from '../../services/category.service';
 import { Transaction } from '../../models/transaction';
@@ -6,6 +6,7 @@ import { AccountService } from '../../services/account.service';
 import { Account } from '../../models/account';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Subject, takeUntil } from 'rxjs';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-overview-transactions',
@@ -13,19 +14,30 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrl: './overview-transactions.component.css',
 })
 export class OverviewTransactionsComponent {
+  @Input() inputAccount?: Account;
   formDisplayed: boolean = false;
-  accountTransactions?: Transaction[];
+  transactions?: Transaction[];
   account?: Account;
   destroy = new Subject<void>();
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private transactionService: TransactionService) {
     accountService.account$.pipe(takeUntil(this.destroy)).subscribe((data) => {
       this.account = data;
+      this.loadTransactions();
     });
+  } 
+  
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
   }
 
-  ngOnInit() {
-    this.loadTransactions();
+  ngOnChanges(): void{
+    if(this.inputAccount){
+      this.destroy.next();
+      this.account = this.inputAccount;
+      this.transactions = this.account.transactions;
+    }
   }
 
   showForm() {
@@ -37,11 +49,31 @@ export class OverviewTransactionsComponent {
   }
 
   loadTransactions() {
-    this.accountTransactions = this.account?.transactions;
+    if(this.account){
+      this.transactions = this.account?.transactions;
+    }
+    
   }
 
-  transactionSaved(String: any) {
+  transactionSaved(event: any) {
     this.showForm();
     this.loadTransactions();
+  }
+
+  deleteTransaction(transaction: Transaction){
+    if(transaction.id != null){
+      this.transactionService.deleteTransaction(transaction.id).subscribe({
+        next: () => {
+          this.accountService.fetchAccounts();
+          console.log('Transaction deleted!')
+        },
+        error: (error) => {
+          console.log('Could not delete Transaction: ', error);
+        }
+      });
+      if(this.inputAccount){
+        this.transactions = this.transactions?.filter(element => element.id =transaction.id);
+      }
+    }
   }
 }
