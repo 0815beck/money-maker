52304463723234fc38e2f0d9e6da48cb5354cbc1
Nico Beck <nicobeck97@gmail.com>
2024-11-26 10:07:38 +0100

    änderungen am account service

--- frontend-money-maker/src/app/services/account.service.ts
+++ frontend-money-maker/src/app/services/account.service.ts
@@ -12,47 +12,83 @@ export class AccountService {
   private account = new BehaviorSubject<Account | undefined>(undefined);
   public account$ = this.account.asObservable();
 
-  public loggedIn$ = this.account$.pipe(map(x => !x === undefined));
+//  public loggedIn$ = this.account$.pipe(map(x => !x === undefined));
 
   private accounts = new BehaviorSubject<Account[] | undefined>(undefined);
   public accounts$ = this.accounts.asObservable();
 
   constructor(private httpClient: HttpClient) {
+//At the start of the application, fetch accounts and choose a default account:    
     httpClient.get<Account[]>(env.baseUrl + '/accounts').subscribe(accounts => {
       this.accounts.next(accounts);
-      //default account auswahl:
       this.account.next(accounts[0]);
-    })
+    });
+
   }
 
 
 
-
-  public addAccount(account: Account): Observable<Account> {
-    this.accounts.next(this.accounts.getValue()?.concat(account));
-    return this.httpClient.post<Account>(env.baseUrl + "/accounts", account)
-  };
-
-  getAccountByID(id: string): Observable<Account>{
-    return this.httpClient.get<Account>(env.baseUrl + "/accounts/" + id)
-  }
-
-
-  deleteAccount(id: number): Observable<void>{
-      return this.httpClient.delete<void>(env.baseUrl + "/accounts/" + id);
-    }
-
+//Methods to change the state of the accounts list variable:
   public fetchAccounts() {
     this.httpClient.get<Account[]>(env.baseUrl + '/accounts').subscribe(accounts => {
       this.accounts.next(accounts);
     })
   }
 
+  public addAccount(account: Account): Observable<Account> {
+    const observable = this.httpClient.post<Account>(env.baseUrl + "/accounts", account);
+    observable.subscribe(account => {
+      this.accounts.next(this.accounts.getValue()?.concat(account));
+      console.log('[Info] A new account has been added to the global account list:', account);
+    });
+    return observable;
+  }
+
+//Bemerkung: Richtiger/sicherer wäre es hier Observable<Account | undefined> zurückzugeben,
+//aber dann müsste die Details-Komponente geändert werden
+  public getAccountByID(id: string): Observable<Account> {
+    const idAsNum = parseInt(id);
+    return this.accounts
+      	.pipe(map(accounts => accounts?.find(a => a.id === idAsNum)))
+        .pipe(switchMap(account => 
+          account === undefined ? 
+            this.httpClient.get<Account>(env.baseUrl + "/accounts/" + id) :  of(account)
+        ));
+  }
+
+  deleteAccount(id: number): Observable<void>{
+    const observable = this.httpClient.delete<void>(env.baseUrl + "/account");
+    observable.subscribe(_ => {
+      console.log('[Info] Account with id ' + id + 'will be removed from the accounts list.');
+      this.accounts.next(this.accounts.getValue()?.filter(x => x.id !== id));
+    })
+    return observable;
+  }
+  
+//Die alten Methoden:
+  /*
+  getAccountByID(id: string): Observable<Account>{
+    return this.httpClient.get<Account>(env.baseUrl + "/accounts/" + id)
+  }
+  */
+  /*
+  public addAccount(account: Account): Observable<Account> {
+    this.accounts.next(this.accounts.getValue()?.concat(account));
+    return this.httpClient.post<Account>(env.baseUrl + "/accounts", account)
+  };
+  */
+
+
+//Methoden, um den ausgewählten account zu steuern:
+
   public setSelectedAccount(id: number) {
-    this.httpClient
+    this.account.next(
+      this.accounts.getValue()?.find(account => account.id === id)
+    )
+/*    this.httpClient
       .get<Account>(env.baseUrl+ '/accounts' + '/' + id).subscribe(account => {
         this.account.next(account);
-      });
+      });*/
   }
 
   public setSelectedAccountToUndefined() {
@@ -64,11 +100,15 @@ export class AccountService {
       .get<Account>(env.baseUrl
         + '/accounts' + '/' + this.account?.getValue()?.id).subscribe(account => {
         this.account.next(account);
-      })
+      });
   }
 
-
- modifyAccount(modifiedAccount: Account): Observable<Account>{
-    return this.httpClient.put<Account>(env.baseUrl+"/accounts", modifiedAccount);
- }
+  modifyAccount(modifiedAccount: Account): Observable<Account> {
+    const observable = this.httpClient.put<Account>(env.baseUrl+"/accounts", modifiedAccount);
+    observable.subscribe(account => {
+      this.accounts.next(this.accounts.getValue()?.map(a => a.id === account.id ? account : a));
+      this.account.next(this.account.getValue()?.id === account.id ? account : this.account.getValue());
+    });
+    return observable;
+  }
 }
