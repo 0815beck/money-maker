4e616eacc320bc955be9764d293bb28ea2deca63
Corinna <corinna.boedigheimer@gmx.de>
2024-11-26 14:41:10 +0100

    Sidebar responsivnes

--- frontend-money-maker/src/app/components/navbar/navbar.component.ts
+++ frontend-money-maker/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+@Input() isSidebarOpen: boolean = false;
 
 }
