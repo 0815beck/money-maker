de9073ad3f50ca44169748c72035371da3d1dff1
Nico Beck <nicobeck97@gmail.com>
2024-11-27 16:06:45 +0100

    removed uneccessary code

--- frontend-money-maker/src/app/components/bar-chart/bar-chart.component.ts
+++ frontend-money-maker/src/app/components/bar-chart/bar-chart.component.ts
@@ -12,10 +12,6 @@ export class BarChartComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-    //this.createChart();
-  }
-
   public chart: any;
 
   ngOnChanges() {
@@ -56,53 +52,5 @@ export class BarChartComponent {
     this.chart = new Chart("MyChart", config);
   }
 
-  /*
-  createChart() {
-    const data: any = {};
-    data.labels = ['January', 'February', 'March', 'April'];
-    data.datasets = [
-      {
-        label: "Income",
-        data: ["500", "200", "600", "20"],
-      }, 
-      {
-        label: "Expenses",
-        data: ["-300", "-240", "-30", "-540"],
-      },
-      {
-        label: "Total",
-        data: ["200", "-40", "570", "-520"],
-      },
-    ]
-
-    //See pie-chart.component.css for the color selection
-    data.datasets[0].backgroundColor = "rgb(9, 185, 24)";
-    data.datasets[1].backgroundColor = "rgb(255, 25, 75)";
-    data.datasets[2].backgroundColor = "rgb(116, 49, 249)";
-
-    const config: any = {};
-    config.type = 'bar';
-    config.data = data;
-    config.options = {};
-    config.options.scales = {
-      x: { beginAtZero: true },
-      y: { beginAtZero: true,},
-    };
-    config.options.indexAxis = 'y';
-    config.options.aspectRatio = 1;
-    config.options.plugins = {};
-    config.options.plugins.title = {
-      display: true,
-      text: "History",
-      font: { size: 18, weight: 'bold' }
-    };
-    config.options.plugins.legend = {
-      display: true,
-      labels: { font: { size: 14 } }
-    };
-
-    this.chart = new Chart("MyChart", config);
-  }
-  */
 }
 
