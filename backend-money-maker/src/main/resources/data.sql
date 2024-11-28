bed4184a94a02176116479f775d99b9dd3ec6ddb
RonjaPieroth <ronja.pieroth@gmail.com>
2024-11-28 15:13:27 +0100

    + new icons

--- backend-money-maker/src/main/resources/data.sql
+++ backend-money-maker/src/main/resources/data.sql
@@ -11,7 +11,7 @@ VALUES ('rent', 'https://img.icons8.com/?size=100&id=4823&format=png&color=00000
        ('groceries', 'https://img.icons8.com/?size=100&id=r1zeTmH4eIib&format=png&color=000000'),
        ('entertainment', 'https://img.icons8.com/?size=100&id=11441&format=png&color=000000'),
        ('clothing',     'https://img.icons8.com/?size=100&id=85007&format=png&color=000000'),
-       ('transportation',     'https://img.icons8.com/?size=100&id=241&format=png&color=000000'),
+       ('transportation',     'https://img.icons8.com/?size=100&id=241&format=png&color=000000');
 
 INSERT INTO fixed_cost (amount, description, start, account_id, category_id)
 VALUES (-450, 'monthly rent', '2024-09-01', 1, 1), (2240, 'salary web dev', '2024-10-03', 1, 2), (-9.99, 'netflix abo', '2024-10-14', 1, 4), (10.25, 'amazon prime abo', '2024-11-28', 1, 4);
