8a8bb6aeb9971aab278bbe85f15721f3288a0c6e
Nico Beck <nicobeck97@gmail.com>
2024-11-29 12:00:28 +0100

    neue tipps

--- frontend-money-maker/src/app/utils/wisdom.ts
+++ frontend-money-maker/src/app/utils/wisdom.ts
@@ -16,9 +16,13 @@ const wisdom: Wisdom[] = [
     {content: ["Der eine wartet, dass die Zeit sich wandelt,", "der andre packt sie an und handelt"], source: "Lindner 1997"},
     {content: ["Rann an die Arbeit, Arbeit bewältigen.", "Probleme sind nur dornige Chancen."], source: "Lindner 1997"},
     {content: ["Auf dem Weg zum Erfolg gibt es keinen Aufzug.", "Man muss schon die Treppen benutzen."], source: "Lindner 1997"},
-    
-    {content: ["Bankraub ist für Amateure", "wahre Profis gründen eine Bank."], source: "nicht Lindner"},
-    {content: ["Die wichtigste Regel beim Shoppen:", "benutz nie dein eigenes Geld."], source: "nicht Lindner"}
+    //{content: ["Bankraub ist für Amateure", "wahre Profis gründen eine Bank."], source: "nicht Lindner"},
+    //{content: ["Die wichtigste Regel beim Shoppen:", "benutz nie dein eigenes Geld."], source: "nicht Lindner"},
+    {content: ["Ich bin reich gewesen, ich bin arm gewesen.", "Reich ist besser."], source: "nicht Lindner"},
+    {content: ["Kauf dir doch heut mal einen Kaffe-To-Go weniger,", "du gieriges Schwein."], source:"nicht Lindner"},
+    {content: ["Wenn du jeden Tag 300€ sparst,", "dann hast du die erste Millionen in 10 Jahren."], source: "nicht Lindner"},
+    {content: ["Sei Alpha, nicht Omega."], source: "nicht Lindner"}
+
 ];
 
 export function getRandomWisdom(): Wisdom {
